99a1a66221ce697fe904b10ebb12b107161cc8b2
farelalthaf <althafannasai@gmail.com>
2025-10-16 19:40:27 +0700

    Update TP03

--- Modul05/TP03.java
+++ Modul05/TP03.java
@@ -1,58 +1,45 @@
 import java.util.Scanner;
-import java.lang.Math;
+
 public class TP03 {
-
     public static void main(String[] args) {
-        final double PI = 3.14;
+        Scanner input = new Scanner(System.in);
+        String namaBangun = input.nextLine();
 
-        Scanner inputScanner = new Scanner(System.in);
-
-        String namaBangun = inputScanner.nextLine();
+        double luas = 0;
+        double keliling = 0;
 
         switch (namaBangun) {
-            case "Persegi": {
-                int sisi = inputScanner.nextInt();
-                int luas = sisi * sisi;
-                int keliling = 4 * sisi;
-                System.out.println(luas + " " + keliling);
+            case "Persegi":
+                int sisi = input.nextInt();
+                luas = sisi * sisi;
+                keliling = 4 * sisi;
+                System.out.printf("%d %d", (int) luas, (int) keliling);
                 break;
-            }
-            case "Persegi Panjang": {
-                int panjang = inputScanner.nextInt();
-                int lebar = inputScanner.nextInt();
-                int luas = panjang * lebar;
-                int keliling = 2 * (panjang + lebar);
-                System.out.println(luas + " " + keliling);
-                break;
-            }
-            case "Segitiga": {
-                double alas = inputScanner.nextDouble();
-                double tinggi = inputScanner.nextDouble();
-                double luas = 0.5 * alas * tinggi;
-                
-                double sisiMiring = Math.sqrt((alas * alas) + (tinggi * tinggi));
-                double keliling = alas + tinggi + sisiMiring;
 
-                if (luas == (long) luas && keliling == (long) keliling) {
-                    System.out.println((long)luas + " " + (long)keliling);
-                } else {
-                    System.out.printf("%.2f %.2f\n", luas, keliling);
-                }
+            case "Persegi Panjang":
+                int panjang = input.nextInt();
+                int lebar = input.nextInt();
+                luas = panjang * lebar;
+                keliling = 2 * (panjang + lebar);
+                System.out.printf("%d %d", (int) luas, (int) keliling);
                 break;
-            }
-            case "Lingkaran": {
-                double diameter = inputScanner.nextDouble();
-                double radius = diameter / 2.0;
-                double luas = PI * radius * radius;
-                double keliling = PI * diameter;
-                System.out.printf("%.2f %.2f\n", luas, keliling);
+
+            case "Segitiga":
+                int alas = input.nextInt();
+                int tinggi = input.nextInt();
+                luas = 0.5 * alas * tinggi;
+                double miring = Math.sqrt(alas * alas + tinggi * tinggi);
+                keliling = alas + tinggi + Math.round(miring);
+                System.out.printf("%d %d", (int) luas, (int) keliling);
                 break;
-            }
-            default:
-                System.out.println("Nama bangun datar tidak valid.");
+
+            case "Lingkaran":
+                int diameter = input.nextInt();
+                double r = diameter / 2.0;
+                luas = 3.14 * r * r;
+                keliling = 3.14 * diameter;
+                System.out.printf("%.2f %.2f", luas, keliling);
                 break;
         }
-        
-        inputScanner.close();
     }
 }
