18aea840bb3a3fbda56c108f0d3195290097af7e
farelalthaf <althafannasai@gmail.com>
2025-11-10 20:36:54 +0700

    Mengumpulkan TP Modul09

--- Modul09/TP01.java
+++ Modul09/TP01.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input sebuah bilangan bulat N yang di mana 1 <= N <= 100
+        int n = input.nextInt();
+
+        // For loop
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " Genap");
+            } else {
+                System.out.println(i + " Ganjil");
+            }
+        }
+
+        // Menutup Scanner
+        input.close();
     }
 }
