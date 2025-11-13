014cb29c0d95c3e7419958d70361f4dba9d58994
farelalthaf <althafannasai@gmail.com>
2025-11-13 15:36:28 +0700

    Mengumpulkan Jurnal Modul09

--- Modul09/Jurnal02.java
+++ Modul09/Jurnal02.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input sebuah bilangan bulat N yang di mana 1 <= N <= 1000
+        int n = input.nextInt();
+
+        // Inisiasi variabel hasil
+        int total = 0;
+
+        // For loop
+        for (int i = 1; i <= n; i+=2) {
+            total += i;
+        }
+
+        // Menampilkan hasil
+        System.out.println(total);
+
+        // Menutup Scanner
+        input.close();
     }
 }
