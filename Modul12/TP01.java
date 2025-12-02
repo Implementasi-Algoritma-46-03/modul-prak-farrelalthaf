71d367fa1b0007396bc4856627ade71318d74f0b
farelalthaf <althafannasai@gmail.com>
2025-12-02 23:34:33 +0700

    Mengumpulkan TP Modul12

--- Modul12/TP01.java
+++ Modul12/TP01.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input sebuah bilangan bulat N di mana 2 <= N <= 10
+        int bilanganBulatN = input.nextInt();
+
+        // Array 2D
+        int matriks[][] = new int[bilanganBulatN][bilanganBulatN];
+        
+        // Inisialisasi variabel total
+        int total = 0;
+        
+        // Nested For
+        for (int i = 0; i < bilanganBulatN; i++) {
+            for (int j = 0; j < bilanganBulatN; j++) {
+            matriks[i][j] = input.nextInt();
+            total += matriks[i][j];
+            }
+        }
+
+
+        // Mencetak hasil
+        System.out.println(total);
+
+        // Menutup Scanner
+        input.close();
     }
 }
--- Modul12/TP02.java
+++ Modul12/TP02.java
@@ -1,6 +1,54 @@
+import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input sebuah bilangan bulat N di mana 2 <= N <= 10
+        int bilanganBulatN = input.nextInt();
+
+        // Array 2D untuk matriks sumber
+        int matriks1[][] = new int[bilanganBulatN][bilanganBulatN];
+        int matriks2[][] = new int[bilanganBulatN][bilanganBulatN];
+        // Array 2D untuk matriks hasil
+        int matriks3[][] = new int[bilanganBulatN][bilanganBulatN];
+        
+        // Nested For untuk membuat matriks1
+        for (int i = 0; i < bilanganBulatN; i++) {
+            for (int j = 0; j < bilanganBulatN; j++) {
+            matriks1[i][j] = input.nextInt();
+            }
+        }
+
+        // Nested For untuk membuat matriks2
+        for (int i = 0; i < bilanganBulatN; i++) {
+            for (int j = 0; j < bilanganBulatN; j++) {
+                matriks2[i][j] = input.nextInt();
+            }
+        }
+
+        // Nested For untuk membuat penjumlahan matriks1 dan matriks2 ke matriks3
+        for (int i = 0; i < bilanganBulatN; i++) {
+            for (int j = 0; j < bilanganBulatN; j++) {
+                matriks3[i][j] = matriks1[i][j] + matriks2[i][j];
+            }
+        }
+        
+        // Nested For untuk mencetak hasil matriks
+        for (int i = 0; i < bilanganBulatN; i++) {
+            for (int j = 0; j < bilanganBulatN; j++) {
+                System.out.print(matriks3[i][j]);
+                if (j < bilanganBulatN - 1) {
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.println();
+        }
+        
+        // Menutup Scanner
+        input.close();
     }
 }
