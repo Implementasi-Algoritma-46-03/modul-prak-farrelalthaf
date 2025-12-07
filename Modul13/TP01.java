7b6073c1e74177e2904d11c583c7746c20275729
farelalthaf <althafannasai@gmail.com>
2025-12-07 21:32:37 +0700

    Update TP01 Modul13

--- Modul13/TP01.java
+++ Modul13/TP01.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class TP01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
 
         // Buat Array
         double[] gelombang1 = new double[10];
