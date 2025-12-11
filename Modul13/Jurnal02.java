f88086581149ad1ff07b3f25ef8be74052bef7b9
farelalthaf <althafannasai@gmail.com>
2025-12-11 15:31:46 +0700

    Mengumpulkan Jurnal Modul13

--- Modul13/Jurnal02.java
+++ Modul13/Jurnal02.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input jumlah barang
+        int jumlahBarang = input.nextInt();
+        input.nextLine(); // Buang new line
+
+        // Input data inventaris dengan menyimpannya di Array
+        String[] data = input.nextLine().split(" ");
+
+        // Insertion Sort
+        insertionSort(data);
+
+        // Menutup Scanner
+        input.close();
+    }
+
+    // Method Insertion Sort
+    public static void insertionSort(String[] data) {
+
+        int jumlahBarang = data.length;
+
+        for (int i = 1; i < jumlahBarang; i++) { // Perulangan dari elemen kedua
+            String key = data[i]; // Elemen yang akan diurutkan
+            int j = i - 1;
+
+            // Geser elemen yang lebih besar dari key ke kanan
+            while (j >= 0 && data[j].compareTo(key) > 0) {
+                data[j + 1 ] = data[j];
+                j = j - 1; 
+            }
+
+            // Masukkan key ke posisi yang benar
+            data[j + 1] = key;
+        }
+
+        // Output hasil sorting
+        for (int i = 0; i < jumlahBarang; i++) {
+            System.out.print(data[i]);
+            if (i < jumlahBarang - 1) System.out.print(" ");
+        }
     }
 }
--- Modul13/Jurnal03.java
+++ Modul13/Jurnal03.java
@@ -1,6 +1,69 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        // Input jumlah mahasiswa
+        int jumlahBarang = input.nextInt();
+        input.nextLine(); // Buang new line
+
+        // Buat Array
+        String[] data = new String[jumlahBarang];
+
+        // Membaca input untuk array
+        for (int i = 0; i < jumlahBarang; i++) {
+            data[i] = input.next(); // Gunakan next() agar membaca kata per kata (seperti contoh soal)
+        }
+
+        // Buang new line
+        input.nextLine();
+
+        // Mengurutkan data
+        Arrays.sort(data);
+
+        // Input key yang dicari
+        String key = input.nextLine();
+
+        // Lakukan pencarian dengan Binary Search
+        int result = binarySearch(data, key);
+
+        // Cek hasil pencarian
+        if (result != -1) {
+            System.out.println("Ditemukan di indeks ke-" + result);
+        } else {
+            System.out.println("Data tidak ditemukan");
+        }
+
+        // Menutup Scanner
+        input.close();
+    }
+
+    public static int binarySearch(String[] data, String key) {
+
+        int low = 0;
+        int high = data.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // Hindari over flow
+
+            // Cek apakah elemen tengah adalah target
+            if (data[mid].equals(key)) {
+                return mid;
+            }
+
+            // Jika target lebih kecil dari elemen tengah, cari di kiri
+            if (key.compareTo(data[mid]) < 0) {
+                high = mid - 1;
+            } else {
+                // Jika target lebih besar, cari di kanan
+                low = mid + 1;
+            }
+        }
+
+        return -1; // Elemen tidak ditemukan
     }
 }
